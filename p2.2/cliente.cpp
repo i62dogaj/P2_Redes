fb4eb758ae9c8162e8b2aa7b3bec4098d532a474
Jose Dominguez Garcia <i62dogaj@uco.es>
2019-10-10 22:14:26 +0200

    Servidor funciona

--- p2.2/cliente.cpp
+++ p2.2/cliente.cpp
@@ -8,8 +8,6 @@
 #include <unistd.h>
 #include <time.h>
 #include <arpa/inet.h>
-#
-
 
 
 int main ( )
@@ -18,25 +16,23 @@ int main ( )
 	/*----------------------------------------------------
 		Descriptor del socket y buffer de datos
 	-----------------------------------------------------*/
-	int s_cliente;
-	struct sockaddr_in servidor;
+	int sd;
+	struct sockaddr_in sockname;
 	char buffer[250];
-	socklen_t long_servidor;
-	fd_set readfds, auxfds;
-	int salida;
-	int fin = 0;
-	int conectado = 0;
-  int opcion;
+	socklen_t len_sockname;
+    fd_set readfds, auxfds;
+    int salida;
+    int fin = 0;
 
 
 	/* --------------------------------------------------
 		Se abre el socket
 	---------------------------------------------------*/
-	s_cliente = socket (AF_INET, SOCK_STREAM, 0);
-	if (s_cliente == -1)
+  	sd = socket (AF_INET, SOCK_STREAM, 0);
+	if (sd == -1)
 	{
 		perror("No se puede abrir el socket cliente\n");
-		exit (1);
+    		exit (1);
 	}
 
 
@@ -45,75 +41,79 @@ int main ( )
 		Se rellenan los campos de la estructura con la IP del
 		servidor y el puerto del servicio que solicitamos
 	-------------------------------------------------------------------*/
-	servidor.sin_family = AF_INET;
-	servidor.sin_port = htons(2000); // puerto
-	servidor.sin_addr.s_addr =  inet_addr("127.0.0.1");
-
+	sockname.sin_family = AF_INET;
+	sockname.sin_port = htons(2000);
+	sockname.sin_addr.s_addr =  inet_addr("127.0.0.1");
 
 	/* ------------------------------------------------------------------
 		Se solicita la conexión con el servidor
 	-------------------------------------------------------------------*/
-	long_servidor = sizeof(servidor);
+	len_sockname = sizeof(sockname);
 
-	if (connect(s_cliente, (struct sockaddr *)&servidor, long_servidor) == -1)
+	if (connect(sd, (struct sockaddr *)&sockname, len_sockname) == -1)
 	{
 		perror ("Error de conexión");
 		exit(1);
 	}
 
-	//Inicializamos las estructuras
-	FD_ZERO(&auxfds);
-	FD_ZERO(&readfds);
+    //Inicializamos las estructuras
+    FD_ZERO(&auxfds);
+    FD_ZERO(&readfds);
 
-	FD_SET(0,&readfds);
-	FD_SET(s_cliente,&readfds);
+    FD_SET(0,&readfds);
+    FD_SET(sd,&readfds);
 
 
 	/* ------------------------------------------------------------------
 		Se transmite la información
 	-------------------------------------------------------------------*/
-	do{
-		auxfds = readfds;
-		salida = select(s_cliente+1,&auxfds,NULL,NULL,NULL);
+	do
+	{
+        auxfds = readfds;
+        salida = select(sd+1,&auxfds,NULL,NULL,NULL);
 
-		//Tengo mensaje desde el servidor
-		if(FD_ISSET(s_cliente, &auxfds)){
+        //Tengo mensaje desde el servidor
+        if(FD_ISSET(sd, &auxfds)){
 
-		    bzero(buffer,sizeof(buffer));
-		    recv(s_cliente,buffer,sizeof(buffer),0);
+            bzero(buffer,sizeof(buffer));
+            recv(sd,buffer,sizeof(buffer),0);
+
+            printf("\n%s\n",buffer);
+
+            if(strcmp(buffer,"Demasiados clientes conectados\n") == 0)
+                fin =1;
+
+            if(strcmp(buffer,"Desconexion servidor\n") == 0)
+                fin =1;
+
+        }
+        else
+        {
+
+            //He introducido información por teclado
+            if(FD_ISSET(0,&auxfds)){
+                bzero(buffer,sizeof(buffer));
+
+                fgets(buffer,sizeof(buffer),stdin);
+
+                if(strcmp(buffer,"SALIR\n") == 0){
+                        fin = 1;
+
+                }
+
+                send(sd,buffer,sizeof(buffer),0);
+
+            }
 
 
-		    if(strcmp(buffer,"Demasiados clientes conectados\n") == 0)
-			fin =1;
-
-		    if(strcmp(buffer,"Desconexion servidor\n") == 0)
-			fin =1;
-
-		}
-		else{
-
-		    //He introducido información por teclado
-		    if(FD_ISSET(0,&auxfds)){
-			bzero(buffer,sizeof(buffer));
-
-			fgets(buffer,sizeof(buffer),stdin);
-
-			if(strcmp(buffer,"SALIR\n") == 0){
-				fin = 1;
-
-			}
-
-			send(s_cliente,buffer,sizeof(buffer),0);
-
-		    }
+        }
 
 
-		}
 
-	}while(fin == 0);
+    }while(fin == 0);
 
-	close(s_cliente);
+    close(sd);
 
-	return 0;
+    return 0;
 
 }
