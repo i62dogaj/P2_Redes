278e6f2f0f99f291d024148b0181dd09db65e6be
Irene Casares Rodríguez <i62caroi@uco.es>
2019-10-11 14:03:47 +0200

    Juego

--- p2.2/ficha.hpp
+++ p2.2/ficha.hpp
@@ -3,22 +3,35 @@
 
 #include <iostream>
 #include <stdio.h>
+#include <stdlib.h>
+#include <ctime>
 using namespace std;
 
 class Ficha{
 
 	private:
 		int nI_; //Numero izquierdo
-		int nD_; //Numero derecho		
-	
+		int nD_; //Numero derecho
+
 	public:
+		//Constructores
+		inline Ficha(){
+			int nI = rand()%6+1;
+			int nD = rand()%6+1;
+			setNI(nI);
+			setND(nD);
+		};
+
 		//OBSERVADORES
 		inline int getNI(){ return nI_;};
 		inline int getND(){ return nD_;};
-		
+
 		//MODIFICADORES
 		inline void setNI(int num){ nI_ = num; };
 		inline void setND(int num){ nD_ = num; };
+
+		//DESTRUCTOR
+		~Ficha(){};
 
 };
 
--- p2.2/jugador.hpp
+++ p2.2/jugador.hpp
@@ -1,1 +1,58 @@
- 
+#ifndef JUGADOR_HPP
+#define JUGADOR_HPP
+
+#include <iostream>
+#include <stdio.h>
+#include <vector>
+#include "ficha.hpp"
+using namespace std;
+
+class Jugador{
+
+	private:
+		int ID_;
+    char login_;
+    char password_;
+    vector <Ficha> mano_;
+
+	public:
+		//Constructor
+    Jugador(){
+      setManoInicial();
+    };
+		inline Jugador(int id, char login, char pass){
+      setID(id);
+      setLogin(login);
+      setPass(pass);
+    };
+
+		//OBSERVADORES
+		inline int getID(){ return ID_;};
+		inline char getLogin(){ return login_;};
+    inline char getPass(){ return password_;};
+    inline vector<Ficha> getMano(){ return mano_;};
+    inline void mostrarMano(){
+      cout << " ";
+      for(int i = 0; i < mano_.size(); i++){
+        cout << "|" << mano_[i].getNI() << "|" << mano_[i].getND() << "| ";
+      }
+    };
+
+		//MODIFICADORES
+		inline void setID(int id){ ID_ = id; };
+		inline void setLogin(char login){ login_ = login; };
+    inline void setPass(char pass){ password_ = pass; }
+    inline void setManoInicial(){
+      for(int i = 0; i < 7; i++){
+        robarFicha();
+      }
+    };
+    inline void robarFicha(){
+      Ficha a;
+      mano_.push_back(a);
+    };
+
+};
+
+
+#endif
